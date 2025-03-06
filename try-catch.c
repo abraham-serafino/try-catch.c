7d48bd6389a3bd446fe1e132fb49af8d70812808
Abraham Serafino <connect@AbrahamSerafino.com>
2025-03-06 00:01:27 -0600

    Add local source code files

---
+++ try-catch.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "try-catch.h"
+
+#define FileException 1
+
+#define new_FileException(message) \
+	new_Exception(FileException, message)
+
+int main(void) {
+    FILE* fp;
+    Exception* e;
+
+    TRY {
+        fp = fopen("./myfile.txt", "w+");
+	fputs("Hello world\n", fp);
+
+	e = new_FileException("Problem writing to file.");
+	THROW(e);
+
+	printf("This message is never shown.");
+
+    } CATCH (FileException) {
+	printf("%s\n", currentException->message);
+
+    } FINALLY {
+	fclose(fp);
+	free(e);
+    }
+}
---
+++ try-catch.h
@@ -0,0 +1,36 @@
+#ifndef _AMS_TRYCATCH_H_
+#define _AMS_TRYCATCH_H_
+
+#include <setjmp.h>
+
+typedef enum exception_t {
+    None
+} ExceptionType;
+
+typedef struct {
+    int type;
+    char* message;
+} Exception;
+
+jmp_buf _buffer;
+
+#define TRY \
+    Exception* currentException = (Exception*) setjmp(_buffer); \
+    if (currentException == 0 || currentException->type == None)
+
+#define CATCH(t) \
+	if (currentException != 0 && currentException->type == t)
+
+#define FINALLY ;
+
+#define THROW(e) longjmp(_buffer, (int) e)
+
+#endif # _AMS_TRYCATCH_H_
+
+Exception* new_Exception(ExceptionType type, char* message) {
+    Exception* e = malloc(sizeof(FileException) + sizeof(message));
+    e->type = type;
+    e->message = message;
+
+    return e;
+}
